41349d17566411af69fba0074f6b54f32e8df708
06shelk <shlee4349@gmail.com>
2024-02-12 15:57:26 +0900

    메인 컨텐츠 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import Today from '../components/contents/Today'
+import Developer from '../components/contents/Developer'
+import Webd from '../components/contents/Webd'
+import Website from '../components/contents/Website'
+import Portfolio from '../components/contents/Portfolio'
+import Youtube from '../components/contents/Youtube'
 
 const Home = () => {
-  return (
-    <Main 
-      title="WEBSYOUTUBE" 
-      description="WEBSYOUTUBE에 오신 것을 환영합니다.">
-        Home
-    </Main>
-  )
+    return (
+        <Main 
+            title = "웹스토리보이 유튜브"
+            description="웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.">
+            
+            <Today />
+            <Developer />
+            <Webd />
+            <Website />
+            <Portfolio />
+            <Youtube />
+        </Main>
+    )
 }
 
 export default Home
